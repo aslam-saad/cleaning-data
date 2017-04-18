ffcf000a6af84c7b7d16d41e14ba5bacc74d61cc
eslam-ghazal <eslamm.gazal@gmail.com>
2017-04-18 23:28:26 +0200

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,3 +1,10 @@
+  ##Script run_analysis.R does the following.
+
+    #Merges the training and the test sets to create one data set.
+    #Extracts only the measurements on the mean and standard deviation for each measurement.
+    #Uses descriptive activity names to name the activities in the data set
+    #Appropriately labels the data set with descriptive variable names.
+    #From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
 
 url <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
 
@@ -29,7 +36,10 @@ if (!require("tidyr")) {
 
 
 ## download the datafile in the working directory, with the name data.zip
-download.file(url, file.path(file_path, "data.zip"))
+if (!file.exists(file.path(file_path, "data.zip"))) {
+  download.file(url, file.path(file_path, "data.zip"))
+}
+
 new_path <- file.path(file_path, "UCI HAR Dataset")
 
 ## read subject data
